11fd851a1f647732aacc96439e3b6570584f39ea
Miguel Torres <mta1830@gmail.com>
2016-08-11 01:38:16 -0400

    Optimización del nuevo componente creado y actualización de las vistas conforme a la actualización

---
+++ config/teachme.php
@@ -0,0 +1,10 @@
+<?php
+
+return array(
+  'menu' => array(
+    'ticket.latest' => 'Pendientes',
+    'ticket.popular' => 'Populares',
+    'ticket.open'    => 'Abiertas',
+    'ticket.closed'  => 'Finalizadas',
+  )
+);
--- resources/views/partials/menu.blade.php
+++ resources/views/partials/menu.blade.php
@@ -1,14 +1,12 @@
 <ul class="nav navbar-nav">
-    <li role="presentation">
-        <a href="http://teachme.dev">Recientes</a>
+  @foreach ($items as $route => $text)
+    <li role="presentation" {!! Html::classes(['active' => Route::is($route)]) !!}>
+    {{-- <li role="presentation" @if(Route::is($route)) class="active" @endif> --}}
+      <a href="{{ route($route) }}">{{ $text }}</a>
     </li>
-    <li role="presentation">
-        <a href="http://teachme.dev/populares">Populares</a>
-    </li>
-    <li role="presentation">
-        <a href="http://teachme.dev/pendientes">Abiertas</a>
-    </li>
-    <li role="presentation">
-        <a href="http://teachme.dev/tutoriales">Finalizadas</a>
-    </li>
+  @endforeach
+  {{-- Excelente forma para incluir datos centralizados --}}
+  {{-- <li role="presentation" @if(route::is('tickets.closed')) class="active" @endif>
+      <a href="{{route('tickets.closed')}}">Finalizadas</a>
+  </li> --}}
 </ul>
