6d412210fa4ca3d12a7fdf01c0fec5c763ce1441
Miguel Torres <mta1830@gmail.com>
2016-08-13 16:06:39 -0400

    Adaptación de la autenticación de usuarios en el sistema

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -38,16 +38,12 @@
 
                 <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 
+                    @if (! Auth::guest())
                     {!!Html::menu('teachme.menu')!!}
+                    @endif
+                    @include('layout.login')
 
-                    <ul class="nav navbar-nav navbar-right">
-                        <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Duilio Palacios <span class="caret"></span></a>
-                            <ul class="dropdown-menu" role="menu">
-                                <li><a href="http://teachme.dev/auth/logout">Logout</a></li>
-                            </ul>
-                        </li>
-                    </ul>
+
                 </div>
             </div>
         </div>
