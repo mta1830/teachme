527cfb20aa19e46c6d423309e840038f5e0e9c24
Miguel Torres <mta1830@gmail.com>
2016-08-28 15:29:41 -0400

    jQuery y Laravel: Eliminar voto con AJAX y refactorización

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -2,31 +2,59 @@ $(document).ready(function () {
 
     var alert = new Alert('#notifications');
 
+    function VoteForm(form, button, buttonRevert) {
+
+      var ticket = button.closest('.ticket');
+      var id = ticket.data('id');
+      var action = form.attr('action').replace(':id', id);
+      var voteCount = ticket.find('.votes-count');
+
+      buttonRevert = ticket.find(buttonRevert);
+
+      button.addClass('hidden');
+
+      this.getVotes = function () {
+        return parseInt(voteCount.text().split('')[0]);
+      };
+
+      this.updateCount = function(votes) {
+        voteCount.text(votes == 1 ? '1 voto' : votes + ' votos');
+      };
+
+      this.submit = function (success) {
+        $.post(action, form.serialize(), function (response) {
+          buttonRevert.removeClass('hidden');
+          success(response);
+        }).fail(function () {
+          button.removeClass('hidden');
+          alert.error('Ocurrió un error :(');
+        });
+      };
+    }
+
     $('.btn-vote').click(function (e) {
         e.preventDefault();
 
-        var form = $('#form-vote');
+        var voteForm = new VoteForm($('#form-vote'),$(this),'.btn-unvote');
 
-        var button = $(this);
-        var ticket = button.closest('.ticket');
-        var id = ticket.data('id');
+        voteForm.submit(function (response) {
+          if (response.success) {
+            alert.success('Gracias por su voto!');
+            voteForm.updateCount(voteForm.getVotes() + 1);
+          }
+        });
+    });
 
-        var action = form.attr('action').replace(':id', id);
+    $('.btn-unvote').click(function (e) {
+        e.preventDefault();
 
-        button.addClass('hidden');
+        var voteForm = new VoteForm($('#form-unvote'), $(this), '.btn-vote');
 
-        $.post(action, form.serialize(), function (response) {
-            //alert
-            //update count votes
-            ticket.find('.btn-unvote').removeClass('hidden');
-
-            alert.success('Gracias port su voto!');
-
-        }).fail(function () {
-            //print error message
-            button.removeClass('hidden');
-
-            alert.error('Ocurrió un error :(');
+        voteForm.submit(function (response) {
+          if (response.success) {
+            alert.success('Voto Eliminado!');
+            voteForm.updateCount(voteForm.getVotes() - 1);
+          }
         });
     });
 
